e2f62e1a54717523232338736f92d1430da5bdcf
petinggint <155607304+petinggint@users.noreply.github.com>
2024-02-25 19:43:26 +0700

    W play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -14,7 +14,7 @@ def track_markup(_, videoid, user_id, channel, fplay):
             ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
-                callback_data=f"MusicStream {videoid}|{user_id}|v|{channel}|{fplay}",
+                callback_data=f"Musicstream {videoid}|{user_id}|v|{channel}|{fplay}",
             ),
         ],
         [
@@ -32,41 +32,36 @@ def stream_markup_timer(_, chat_id, played, dur):
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
-    if 0 < umm <= 10:
-        bar = "◉—————————"
-    elif 10 < umm < 20:
-        bar = "—◉————————"
+    if 0 < umm < 10:
+        bar = "¤————————"
+    elif 10 <= umm < 20:
+        bar = "—¤———————"
     elif 20 <= umm < 30:
-        bar = "——◉———————"
+        bar = "——¤——————"
     elif 30 <= umm < 40:
-        bar = "———◉——————"
+        bar = "———¤—————"
     elif 40 <= umm < 50:
-        bar = "————◉—————"
+        bar = "————¤————"
     elif 50 <= umm < 60:
-        bar = "—————◉————"
-    elif 60 <= umm < 70:
-        bar = "——————◉———"
-    elif 70 <= umm < 80:
-        bar = "———————◉——"
+        bar = "—————¤———"
+    elif 60 <= umm < 80:
+        bar = "——————¤——"
     elif 80 <= umm < 95:
-        bar = "————————◉—"
-    else:
-        bar = "—————————◉"
+        bar = "———————¤—"
     buttons = [
-        [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
         [
             InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
                 callback_data="GetTimer",
             )
         ],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+        [
+            InlineKeyboardButton(text="✫ ᴅᴇᴠ ✫", url=f"https://t.me/Foundermidnight"),
+            InlineKeyboardButton(text="✫ ᴄʜᴀɴɴᴇʟ ✫", url=f"http://t.me/Ortresxz"),
+        ],
+        [
+            InlineKeyboardButton(text= "✫ ᴅᴏɴᴀsɪ ✫", url=f"https://link.dana.id/qr/2gmos5bu"),
+        ],
     ]
     return buttons
 
@@ -74,13 +69,12 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="✫ ᴅᴇᴠ ✫", url=f"https://t.me/Foundermidnight"),
+            InlineKeyboardButton(text="✫ ᴄʜᴀɴɴᴇʟ ✫", url=f"http://t.me/Ortresxz"),
         ],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+        [
+            InlineKeyboardButton(text= "✫ ᴅᴏɴᴀsɪ ✫", url=f"https://link.dana.id/qr/2gmos5bu"),
+        ],
     ]
     return buttons
 
