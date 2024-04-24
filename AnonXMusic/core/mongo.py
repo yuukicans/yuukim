16d433ee09d3842943bb84a945318ead95122e08
petinggint <155607304+petinggint@users.noreply.github.com>
2024-04-24 18:29:23 +0700

    FIKS mongo.py

--- AnonXMusic/core/mongo.py
+++ AnonXMusic/core/mongo.py
@@ -4,11 +4,11 @@ from config import MONGO_DB_URI
 
 from ..logging import LOGGER
 
-LOGGER(__name__).info("lagi gw konekin dulu ke mongo...")
+LOGGER(__name__).info("ʟᴀɢɪ ɢᴡ ᴋᴏɴᴇᴋɪɴ ᴅᴜʟᴜ ᴋᴇ ᴍᴏɴɢᴏ...")
 try:
     _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
     mongodb = _mongo_async_.Anon
     LOGGER(__name__).info("dah konek ke mongo.")
 except:
-    LOGGER(__name__).error("Gagal Konek Mek, Ke database Mongo lu.")
+    LOGGER(__name__).error("ɢᴀɢᴀʟ ᴋᴏɴᴇᴋ ᴍᴇᴋ, ᴋᴇ ᴅᴀᴛᴀʙᴀsᴇ ᴍᴏɴɢᴏ ʟᴜ.")
     exit()
