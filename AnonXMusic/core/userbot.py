cf666c07ba753dd82d77322a4205e1e1e16ef4c0
petinggint <155607304+petinggint@users.noreply.github.com>
2024-05-23 21:29:34 +0700

    BA userbot.py

--- AnonXMusic/core/userbot.py
+++ AnonXMusic/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("Berlinmusic_support")
-                await self.one.join_chat("areanightbest")
+                await self.one.join_chat("blackangelsmusic")
+                await self.one.join_chat("allnightarea")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("Berlinmusic_support")
-                await self.two.join_chat("areanightbest")
+                await self.two.join_chat("blackangelsmusic")
+                await self.two.join_chat("allnightarea")
             except:
                 pass
             assistants.append(2)
@@ -93,8 +93,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("Berlinmusic_support")
-                await self.three.join_chat("areanightbest")
+                await self.three.join_chat("blackangelsmusic")
+                await self.three.join_chat("allnightarea")
             except:
                 pass
             assistants.append(3)
@@ -114,8 +114,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("Berlinmusic_support")
-                await self.four.join_chat("areanightbest")
+                await self.four.join_chat("blackangelsmusic")
+                await self.four.join_chat("allnightarea")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("Berlinmusic_support")
-                await self.five.join_chat("areanightbest")
+                await self.five.join_chat("blackangelsmusic")
+                await self.five.join_chat("allnightarea")
             except:
                 pass
             assistants.append(5)
