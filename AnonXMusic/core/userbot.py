f487a794ea8aecc729b458700311a10ab151eb37
petinggint <155607304+petinggint@users.noreply.github.com>
2024-06-19 02:52:37 +0700

    A userbot.py

--- AnonXMusic/core/userbot.py
+++ AnonXMusic/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("yukasamusic")
-                await self.one.join_chat("yukasateam")
+                await self.one.join_chat("musicwandasupport")
+                await self.one.join_chat("musicwandasupport")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("yukasamusic")
-                await self.two.join_chat("yukasateam")
+                await self.two.join_chat("musicwandasupport")
+                await self.two.join_chat("musicwandasupport")
             except:
                 pass
             assistants.append(2)
@@ -93,8 +93,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("yukasamusic")
-                await self.three.join_chat("yukasateam")
+                await self.three.join_chat("musicwandasupport")
+                await self.three.join_chat("musicwandasupport")
             except:
                 pass
             assistants.append(3)
@@ -114,8 +114,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("yukasamusic")
-                await self.four.join_chat("yukasateam")
+                await self.four.join_chat("musicwandasupport")
+                await self.four.join_chat("musicwandasupport")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("yukasamusic")
-                await self.five.join_chat("yukasateam")
+                await self.five.join_chat("musicwandasupport")
+                await self.five.join_chat("musicwandasupport")
             except:
                 pass
             assistants.append(5)
