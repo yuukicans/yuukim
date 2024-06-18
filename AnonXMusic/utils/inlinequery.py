82e8d4f192ac67f1c2bbc25192b61fca75e7b454
petinggint <155607304+petinggint@users.noreply.github.com>
2024-06-19 02:57:10 +0700

    A inlinequery.py

--- AnonXMusic/utils/inlinequery.py
+++ AnonXMusic/utils/inlinequery.py
@@ -7,37 +7,37 @@ answer.extend(
         InlineQueryResultArticle(
             title="Pᴀᴜsᴇ",
             description=f"ᴅᴀʜ ɢᴡ ᴊᴇᴅᴀ ʏᴀ ᴍᴇᴋ sᴛʀᴇᴀᴍ ɴʏᴀ.",
-            thumb_url="https://telegra.ph//file/fbe9446cf00b064f27f96.jpg",
+            thumb_url="https://telegra.ph//file/11271f123cf063a701a96.jpg",
             input_message_content=InputTextMessageContent("/pause"),
         ),
         InlineQueryResultArticle(
             title="Rᴇsᴜᴍᴇ",
             description=f"ᴅᴀʜ ɢᴡ ʀᴇsᴜᴍᴇ ʏᴀ ᴍᴇᴋ sᴛʀᴇᴀᴍ ɴʏᴀ.",
-            thumb_url="https://telegra.ph//file/fbe9446cf00b064f27f96.jpg",
+            thumb_url="https://telegra.ph//file/11271f123cf063a701a96.jpg",
             input_message_content=InputTextMessageContent("/resume"),
         ),
         InlineQueryResultArticle(
             title="Sᴋɪᴩ",
             description=f"ᴅᴀʜ ɢᴡ sᴋɪᴘ ʏᴀ ᴍᴇᴋ sᴛʀᴇᴀᴍ ɴʏᴀ.",
-            thumb_url="https://telegra.ph//file/fbe9446cf00b064f27f96.jpg",
+            thumb_url="https://telegra.ph//file/11271f123cf063a701a96.jpg",
             input_message_content=InputTextMessageContent("/skip"),
         ),
         InlineQueryResultArticle(
             title="Eɴᴅ",
             description="ᴅᴀʜ ɢᴡ ᴇɴᴅ ʏᴀ ᴍᴇᴋ sᴛʀᴇᴀᴍ ɴʏᴀ.",
-            thumb_url="https://telegra.ph//file/fbe9446cf00b064f27f96.jpg",
+            thumb_url="https://telegra.ph//file/11271f123cf063a701a96.jpg",
             input_message_content=InputTextMessageContent("/end"),
         ),
         InlineQueryResultArticle(
             title="Sʜᴜғғʟᴇ",
             description="ᴅᴀʜ ɢᴡ sᴜғғʟᴇ ʏᴀ ᴍᴇᴋ sᴛʀᴇᴀᴍ ɴʏᴀ.",
-            thumb_url="https://telegra.ph//file/fbe9446cf00b064f27f96.jpg",
+            thumb_url="https://telegra.ph//file/11271f123cf063a701a96.jpg",
             input_message_content=InputTextMessageContent("/shuffle"),
         ),
         InlineQueryResultArticle(
             title="Lᴏᴏᴩ",
             description="ᴅᴀʜ ɢᴡ ʟᴏᴏᴘ ʏᴀ ᴍᴇᴋ sᴛʀᴇᴀᴍ ɴʏᴀ.",
-            thumb_url="https://telegra.ph//file/fbe9446cf00b064f27f96.jpg",
+            thumb_url="https://telegra.ph//file/11271f123cf063a701a96.jpg",
             input_message_content=InputTextMessageContent("/loop 3"),
         ),
     ]
