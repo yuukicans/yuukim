3624467c22062983049ea02f5c3bc4cf15b69728
petinggint <155607304+petinggint@users.noreply.github.com>
2024-06-19 23:59:10 +0700

    A config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 14400))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", -1002149357186))
+LOGGER_ID = int(getenv("LOGGER_ID", ))
 
 # Get this value from @Berlinmusicbot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 7353842230))
