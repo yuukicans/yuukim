1489734ace216b0f8fd3fec3c7067bcc0f77cbb5
petinggint <155607304+petinggint@users.noreply.github.com>
2024-11-04 15:24:33 +0700

    SA config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 14400))
 LOGGER_ID = int(getenv("LOGGER_ID", -1002110009271))
 
 # Get this value from @Berlinmusicbot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 7390960414))
+OWNER_ID = int(getenv("OWNER_ID", 769398602))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
