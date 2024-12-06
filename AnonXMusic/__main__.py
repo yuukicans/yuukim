79719953904fb92282e5faf63c1b6089408975fc
petinggint <155607304+petinggint@users.noreply.github.com>
2024-12-06 10:06:58 +0700

    L __main__.py

--- AnonXMusic/__main__.py
+++ AnonXMusic/__main__.py
@@ -21,7 +21,7 @@ async def init():
         and not config.STRING4
         and not config.STRING5
     ):
-        LOGGER(__name__).error("Assistant Saka client variables not defined, exiting...")
+        LOGGER(__name__).error("Assistant leon client variables not defined, exiting...")
         exit()
     await sudo()
     try:
@@ -42,20 +42,20 @@ async def init():
     try:
         await Anony.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
-        LOGGER("sakaxmusic").error(
+        LOGGER("leonxmusic").error(
             " ᴍᴀᴜ ɢᴡ ɪᴅᴜᴘɪɴ ɢᴀ ɴɪʜ ʙᴏᴛɴʏᴀ, ɴʏᴀʟᴀɪɴ ᴍᴏɴʏᴇᴛ ᴏs ɴʏᴀ.\n\nStopping Bot..."
         )
         exit()
     except:
         pass
     await Anony.decorators()
-    LOGGER("sakaxmusic").info(
+    LOGGER("leonxmusic").info(
         "\x41\x6e\x6f\x6e\x58\x20\x4d\x75\x73\x69\x63\x20\x42\x6f\x74\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\n\n\x44\x6f\x6e'\x74\x20\x66\x6f\x72\x67\x65\x74\x20\x74\x6f\x20\x76\x69\x73\x69\x74\x20\x40\x46\x61\x6c\x6c\x65\x6e\x41\x73\x73\x6f\x63\x69\x61\x74\x69\x6f\x6e"
     )
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("sakaxmusic").info("Stopping saka x Music Bot...")
+    LOGGER("leonxmusic").info("Stopping leon x Music Bot...")
 
 
 if __name__ == "__main__":
