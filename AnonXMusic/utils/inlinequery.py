333c5fc135c00b1633620149382fa6e73e754b91
petinggint <155607304+petinggint@users.noreply.github.com>
2025-02-23 14:51:34 +0700

    C inlinequery.py

--- AnonXMusic/utils/inlinequery.py
+++ AnonXMusic/utils/inlinequery.py
@@ -7,37 +7,37 @@ answer.extend(
         InlineQueryResultArticle(
             title="Pᴀᴜsᴇ",
             description=f"ᴅᴀʜ ɢᴡ ᴊᴇᴅᴀ ʏᴀ ᴍᴇᴋ sᴛʀᴇᴀᴍ ɴʏᴀ.",
-            thumb_url="https://itzpire.com/file/704da060f1d2.jpg",
+            thumb_url="https://files.catbox.moe/ci0fhz.jpg",
             input_message_content=InputTextMessageContent("/pause"),
         ),
         InlineQueryResultArticle(
             title="Rᴇsᴜᴍᴇ",
             description=f"ᴅᴀʜ ɢᴡ ʀᴇsᴜᴍᴇ ʏᴀ ᴍᴇᴋ sᴛʀᴇᴀᴍ ɴʏᴀ.",
-            thumb_url="https://itzpire.com/file/704da060f1d2.jpg",
+            thumb_url="https://files.catbox.moe/ci0fhz.jpg",
             input_message_content=InputTextMessageContent("/resume"),
         ),
         InlineQueryResultArticle(
             title="Sᴋɪᴩ",
             description=f"ᴅᴀʜ ɢᴡ sᴋɪᴘ ʏᴀ ᴍᴇᴋ sᴛʀᴇᴀᴍ ɴʏᴀ.",
-            thumb_url="https://itzpire.com/file/704da060f1d2.jpg",
+            thumb_url="https://files.catbox.moe/ci0fhz.jpg",
             input_message_content=InputTextMessageContent("/skip"),
         ),
         InlineQueryResultArticle(
             title="Eɴᴅ",
             description="ᴅᴀʜ ɢᴡ ᴇɴᴅ ʏᴀ ᴍᴇᴋ sᴛʀᴇᴀᴍ ɴʏᴀ.",
-            thumb_url="https://itzpire.com/file/704da060f1d2.jpg",
+            thumb_url="https://files.catbox.moe/ci0fhz.jpg",
             input_message_content=InputTextMessageContent("/end"),
         ),
         InlineQueryResultArticle(
             title="Sʜᴜғғʟᴇ",
             description="ᴅᴀʜ ɢᴡ sᴜғғʟᴇ ʏᴀ ᴍᴇᴋ sᴛʀᴇᴀᴍ ɴʏᴀ.",
-            thumb_url="https://itzpire.com/file/704da060f1d2.jpg",
+            thumb_url="https://files.catbox.moe/ci0fhz.jpg",
             input_message_content=InputTextMessageContent("/shuffle"),
         ),
         InlineQueryResultArticle(
             title="Lᴏᴏᴩ",
             description="ᴅᴀʜ ɢᴡ ʟᴏᴏᴘ ʏᴀ ᴍᴇᴋ sᴛʀᴇᴀᴍ ɴʏᴀ.",
-            thumb_url="https://itzpire.com/file/704da060f1d2.jpg",
+            thumb_url="https://files.catbox.moe/ci0fhz.jpg",
             input_message_content=InputTextMessageContent("/loop 3"),
         ),
     ]
