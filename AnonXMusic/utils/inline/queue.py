a0722a43ddb8949e43c01fc889cf2419c114c13a
petinggint <155607304+petinggint@users.noreply.github.com>
2025-02-23 14:52:22 +0700

    C queue.py

--- AnonXMusic/utils/inline/queue.py
+++ AnonXMusic/utils/inline/queue.py
@@ -66,7 +66,7 @@ def queue_back_markup(_, CPLAY):
 def aq_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="developer", url=f"https://t.me/Kiilleon"),
+            InlineKeyboardButton(text="developer", url=f"https://t.me/UcalMeVin"),
         ],
     ]
     return buttons
