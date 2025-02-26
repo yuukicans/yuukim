1342b070b631ce19dc6654e8710fa0e717910f19
petinggint <155607304+petinggint@users.noreply.github.com>
2025-02-26 22:41:01 +0700

    C config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/petinggint/Musicnt",
+    "https://github.com/petinggint/musicvin",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
