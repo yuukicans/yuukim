3f8138e5a3000189549002e54f8b35c806926a46
yuukicans <calvinmanagement15@gmail.com>
2025-03-15 13:21:37 +0700

    yuuki __main__.py

--- AnonXMusic/__main__.py
+++ AnonXMusic/__main__.py
@@ -55,7 +55,7 @@ async def init():
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("leonxmusic").info("Stopping Calvin x Music Bot...")
+    LOGGER("leonxmusic").info("Stopping Yuuki x Music Bot...")
 
 
 if __name__ == "__main__":
